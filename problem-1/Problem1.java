67e63dea4dc8b2529f88224ff9d3c39d6f67aa1d
Daniel Grant <daniel.s.grant12@gmail.com>
2014-08-25 11:06:32 -0700

    Java p1 solution

---
+++ problem-1/Problem1.java
@@ -0,0 +1,16 @@
+/* Problem1.java 
+   Daniel grant
+   Java solution to Problem 1
+*/
+
+class Problem1 {
+    public static void main(String[] args) {
+	int total = 0;
+	for (int i = 0; i < 1000; i++) {
+	    if (i % 3 == 0 || i % 5 == 0) {
+		total += i;
+	    }
+	}
+	System.out.println(total);
+    }
+}
